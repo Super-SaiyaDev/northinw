ebf28c9c9e8d49cccf5fff29cf53bc3d621c74fb
edelson <edelsonjaelat@gmail.com>
2023-11-17 11:06:21 -0400

    practicando con react

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+import { Componente } from './componentes/componente';
+function App() {
+  return (
+    <div className="container">   
+    <Componente/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/componente.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+export function Componente(props) {
+
+    return (
+        <div className="contenedor">
+
+            <div class="slider">
+
+                <img src="imagen1.jpg" alt="Imagen 1" />
+                <img src="imagen2.jpg" alt="Imagen 2" />
+                <img src="imagen3.jpg" alt="Imagen 3" />
+            </div>
+
+            <div className="menu">
+                <div className="container-menu">
+                    <div className="cont-menu">
+                        <nav>
+                            <a href="#" className="links">links</a>
+                            <a href="#" className="links">links</a>
+                            <a href="#" className="links">links</a>
+                            <a href="#" className="links">links</a>
+                            <a href="#" className="links">links</a>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    );
+
+}
+
